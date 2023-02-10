be080cca1f21c5440e2da0f2a59394f7d6ffd7d4
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-02-10 10:48:19 -0800

    clean up

--- frost-test/src/event.rs
+++ frost-test/src/event.rs
@@ -4,41 +4,40 @@ use wtfrost::{
     Scalar,
 };
 
-// Each signer, DKG and message have a unique id.
-pub type Id = [u32; 8];
+/// 
+pub trait Protocol {
+    type DkgId;
+    type ParticipantId;
+    type SignatureId;
+}
 
 /// Should be sent by DKG coordinator.
 /// In theory, a signer can participate in multiple DKGs and be
 /// responsible for multiple parties (DKG and signature shares).
-pub struct DkgBegin {
-    pub dkg_id: Id,
+pub struct DkgBegin<P: Protocol> {
+    pub dkg_id: P::DkgId,
     pub N: u32,
     pub T: u32,
     /// Must have a size of N
-    pub party_to_signer_map: Vec<Id>,
+    pub party_to_signer_map: Vec<P::ParticipantId>,
 }
 
 /// Should be send by each signer after `DkgBegin`
 /// A signer should send multiple events if multiple parties were assigned.
-pub struct DkgPolyCommitment {
-    pub dkg_id: Id,
+pub struct DkgShare<P: Protocol> {
+    pub dkg_id: P::DkgId,
     pub party_id: u32,
-    pub value: PolyCommitment,
-}
-
-/// Should be send by each signer after all `DkgPolyCommitment` are collected.
-/// A signer should send multiple events if multiple parties were assigned.
-pub struct DkgShare {
-    pub dkg_id: Id,
-    pub party_id: u32,
-    pub value: Vec<(u32, Scalar)>,
+    pub public: PolyCommitment,
+    /// The size of the vector should be `N-1`. 
+    /// They are messages for all other parties.
+    pub private: Vec<Scalar>,
 }
 
 /// Should be sent by a signature aggregator.
 /// In theory, a signer can be involved in signing multiple messages at the same time.
-pub struct Sign {
-    pub dkg_id: Id,
-    pub signature_id: Id,
+pub struct Sign<P: Protocol> {
+    pub dkg_id: P::DkgId,
+    pub signature_id: P::SignatureId,
     // It's a set of T parties so it can be a vector of bits.
     pub parties: Vec<u32>,
     pub message: Vec<u8>,
@@ -46,25 +45,24 @@ pub struct Sign {
 
 /// Should be send by a signer.
 /// A signer should send multiple events if multiple parties were assigned.
-pub struct Nonce {
-    pub signature_id: Id,
+pub struct Nonce<P: Protocol> {
+    pub signature_id: P::DkgId,
     pub party_id: u32,
     pub nonce: PublicNonce,
 }
 
 /// Should be send by a signer.
 /// A signer should send multiple events if multiple parties were assigned.
-pub struct Share {
-    pub message_id: Id,
+pub struct Share<P: Protocol> {
+    pub signature_id: P::SignatureId,
     pub party_id: u32,
     pub share: SignatureShare,
 }
 
-pub enum Event {
-    DkgBegin(DkgBegin),
-    DkgPolyCommitment(DkgPolyCommitment),
-    DkgShare(DkgShare),
-    Sign(Sign),
-    Nonce(Nonce),
-    Share(Share),
+pub enum Event<P: Protocol> {
+    DkgBegin(DkgBegin<P>),
+    DkgShare(DkgShare<P>),
+    Sign(Sign<P>),
+    Nonce(Nonce<P>),
+    Share(Share<P>),
 }
