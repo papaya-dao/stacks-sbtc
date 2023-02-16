2a885a9437315205b26846d48773e67a98562d9d
Don Park <donp@donp.org>
2023-02-15 17:16:05 -0800

    this turned into the threaded/socket based version

--- frost-test/tests/frost-btc.rs
+++ frost-test/tests/frost-btc.rs
@@ -1,46 +1,56 @@
+use frost_signer::net::{HttpNet, HttpNetListen, Message, NetListen};
+use frost_signer::signing_round::{DkgEnd, MessageTypes, SigningRound};
+use std::future::Future;
 use std::net::TcpListener;
+use std::slice::Iter;
+use std::{thread, time};
 use std::thread::spawn;
-use frost_signer::net::{HttpNet, HttpNetListen};
-use frost_signer::signing_round::{MessageTypes, SigningRound};
+
+const relay_addr: &str = "127.0.0.1:9776";
 
 //
 #[test]
 fn frost_dkg_btc() {
-    spawn (|| { relay_server() });
+    spawn(|| relay_server());
 
     let t = 3; // Threshold
     let n = 6; // Total
-    let signers = vec![
+
+    // Signers
+    let mut signers = vec![
         SigningRound::new(t, n, 1, vec![0, 1]),
         SigningRound::new(t, n, 2, vec![2, 3]),
         SigningRound::new(t, n, 3, vec![4, 5]),
     ];
 
-    // DKG
-    let net: HttpNet = HttpNet::new(config.common.stacks_node_url.clone());
+    // Signers networking
+    let net: HttpNet = HttpNet::new(relay_addr.to_owned());
+    let mut net_queue = HttpNetListen::new(net.clone(), vec![]);
 
-    //broadcast(signers, msg);
+    // DKG
+
+    let mut in_msgs = vec![];
+    for signer in signers.iter() {
+        net_queue.poll(signer.signer.signer_id);
+        match net_queue.next_message() {
+            None => {
+                println!("signer #{} poll empty", signer.signer.signer_id);
+                thread::sleep(time::Duration::from_millis(100)) },
+            Some(msg) => in_msgs.push(msg),
+        }
+    }
+    let mut out_msgs = vec![];
+    for msg in in_msgs {
+        signers = deliver(signers, &msg.msg, &mut out_msgs);
+    }
+
     // Peg-in: spend a P2PKH utxo and lock it into P2TR output script using the frost public aggregate key.
 
     // Peg-out: spend the output from the Peg-in tx using frost sign.
 }
 
-fn next_message(mut net: HttpNet,  id: u32) {
-    net.poll(id);
-    match net.next_message() {
-        None => {
-            thread::sleep(time::Duration::from_millis(500));
-        }
-        Some(m) => {
-            tx.send(m).unwrap();
-        }
-    };
-
-}
-
 fn relay_server() {
-    let addr = "127.0.0.1:9776";
-    let listener = TcpListener::bind(addr).unwrap();
+    let listener = TcpListener::bind(relay_addr).unwrap();
     let mut server = relay_server::Server::default();
     for stream_or_error in listener.incoming() {
         let f = || server.update(&mut stream_or_error?);
@@ -50,10 +60,14 @@ fn relay_server() {
     }
 }
 
-fn broadcast(recipients: Vec<SigningRound>, msg: MessageTypes) {
-    let mut out_queue : Vec<MessageTypes> =  vec![];
-    for mut recipient in recipients {
-        let mut out_msgs = recipient.process(&msg).unwrap();
-        out_queue.append(&mut out_msgs);
+fn deliver(
+    mut recipients: Vec<SigningRound>,
+    msg: &MessageTypes,
+    out_msgs: &mut Vec<MessageTypes>,
+) -> Vec<SigningRound> {
+    for recipient in recipients.iter_mut() {
+        let mut new_msgs = recipient.process(msg).unwrap();
+        out_msgs.append(&mut new_msgs);
     }
+    recipients
 }
