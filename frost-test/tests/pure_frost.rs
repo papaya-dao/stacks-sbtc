a82c90638f3f3594234bd2c3239fd48a96a84a00
Don Park <donp@donp.org>
2023-02-17 14:30:32 -0800

    start peg_out bits

--- frost-test/tests/pure_frost.rs
+++ frost-test/tests/pure_frost.rs
@@ -1,5 +1,6 @@
-use bitcoin::schnorr::UntweakedPublicKey;
-use bitcoin::{PackedLockTime, Script, Transaction};
+use bitcoin::{PackedLockTime, Script, Transaction, XOnlyPublicKey};
+use bitcoin::consensus::Encodable;
+use bitcoin::secp256k1::PublicKey;
 use hashbrown::HashMap;
 use rand_core::OsRng;
 use wtfrost::common::{PolyCommitment, Signature};
@@ -23,31 +24,52 @@ fn pure_frost_test() {
         v1::Signer::new(&[3], N, T, &mut rng),
     ];
 
+    let secp = bitcoin::secp256k1::Secp256k1::new();
+    let user_keys = secp.generate_keypair(&mut OsRng::default());
+    //let user_keys = bitcoin::util::key::KeyPair::new(&secp, &mut OsRng::default());
+
     // DKG (Distributed Key Generation)
     let (public_key_shares, group_key) = dkg_round(&mut rng, &mut signers);
 
-
+    group_key.compress();
     // Peg Wallet Address from group key
-    let mut public_key_bytes_x = group_key.gej.x.n.map(|x64| x64.to_le_bytes()).into_iter().flatten().collect::<Vec<u8>>();
-    println!("group key X: (len {}) {:?}", public_key_bytes_x.len(), public_key_bytes_x);
-    let peg_wallet_address = bitcoin::util::key::XOnlyPublicKey::from_slice(&public_key_bytes_x).unwrap();
+    let peg_wallet_address = bitcoin::secp256k1::PublicKey::from_slice(&group_key.compress().as_bytes()).unwrap();
 
     // Send to stx address
     let stx_address = [0; 32];
     let peg_in = build_peg_in(1000, peg_wallet_address, stx_address);
+    let mut peg_in_bytes: Vec<u8> = vec![];
+    peg_in.consensus_encode(&mut peg_in_bytes).unwrap();
+    println!("peg-in tx: {:?}", peg_in_bytes);
 
+    let peg_out = build_peg_out(1000, user_keys);
     // signing. Signers: 0 (parties: 0, 1) and 1 (parties: 2)
-    let result = signing_round(T, N, &mut rng, &mut signers, public_key_shares);
+    let mut peg_out_bytes: Vec<u8> = vec![];
+    let peg_out_bytes_len = peg_out.consensus_encode(&mut peg_out_bytes).unwrap();
+    let result = signing_round(&peg_out_bytes, T, N, &mut rng, &mut signers, public_key_shares);
 
     assert!(result.is_ok());
 }
 
+fn build_peg_out(
+    satoshis: u64,
+    user_address: PublicKey,
+) -> Transaction {
+    bitcoin::blockdata::transaction::Transaction {
+        version: 0,
+        lock_time: PackedLockTime(0),
+        input: vec![],
+        output: vec![],
+    }
+}
+
 fn build_peg_in(
     satoshis: u64,
-    peg_wallet_address: UntweakedPublicKey,
+    peg_wallet_address: PublicKey,
     stx_address: [u8; 32],
 ) -> Transaction {
     let secp = bitcoin::util::key::Secp256k1::new();
+
     // Peg-In TX
     let peg_in_input = bitcoin::TxIn {
         previous_output: Default::default(),
@@ -55,7 +77,7 @@ fn build_peg_in(
         sequence: Default::default(),
         witness: Default::default(),
     };
-    let taproot = Script::new_v1_p2tr(&secp, peg_wallet_address, None);
+    let taproot = Script::new_v1_p2tr(&secp, XOnlyPublicKey::from(peg_wallet_address), None);
     let peg_in_output = bitcoin::TxOut {
         value: satoshis,
         script_pubkey: taproot,
@@ -69,6 +91,7 @@ fn build_peg_in(
 }
 
 fn signing_round(
+    message: &[u8],
     T: usize,
     N: usize,
     mut rng: &mut OsRng,
@@ -77,8 +100,6 @@ fn signing_round(
 ) -> Result<Signature, AggregatorError> {
     // decide which signers will be used
     let mut signers = [signers[0].clone(), signers[1].clone()];
-
-    const MSG: &[u8] = "It was many and many a year ago".as_bytes();
 
     // get nonces and shares
     let (nonces, shares) = {
@@ -91,7 +112,7 @@ fn signing_round(
         // get shares
         let shares = signers
             .iter()
-            .flat_map(|s| s.sign(MSG, &ids, &nonces))
+            .flat_map(|s| s.sign(message, &ids, &nonces))
             .collect::<Vec<_>>();
 
         (nonces, shares)
@@ -99,7 +120,7 @@ fn signing_round(
 
     SignatureAggregator::new(N, T, A.clone())
         .unwrap()
-        .sign(&MSG, &nonces, &shares)
+        .sign(&message, &nonces, &shares)
 }
 
 fn dkg_round(mut rng: &mut OsRng, signers: &mut [v1::Signer; 3]) -> (Vec<PolyCommitment>, wtfrost::Point) {
