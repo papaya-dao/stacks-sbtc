98687084f233618a1ecf7ab04ace418fe11acd24
Don Park <donp@donp.org>
2023-03-12 15:12:26 -0700

    use blockstack's PoxAddress.to_bitcoin_tx_out; add test

--- stacks-coordinator/src/peg_wallet.rs
+++ stacks-coordinator/src/peg_wallet.rs
@@ -1,4 +1,6 @@
+use bitcoin::hashes::hex::{FromHex, ToHex};
 use bitcoin::hashes::Hash;
+use bitcoin::Script;
 use serde::Serialize;
 
 use crate::bitcoin_node;
@@ -68,7 +70,6 @@ pub struct FileBitcoinWallet {}
 
 impl BitcoinWallet for FileBitcoinWallet {
     fn fulfill_peg_out(&self, op: &PegOutRequestOp) -> BitcoinTransaction {
-        //fn build_peg_out(satoshis: u64, user_address: bitcoin::PublicKey, utxo: OutPoint) -> Transaction
         let bitcoin_txid = bitcoin::Txid::from_slice(op.txid.as_bytes()).unwrap();
         let utxo = bitcoin::OutPoint {
             txid: bitcoin_txid,
@@ -80,12 +81,12 @@ impl BitcoinWallet for FileBitcoinWallet {
             sequence: Default::default(),
             witness: Default::default(),
         };
-        // todo: op.recipient should be a bitcoin address
-        let user_address = bitcoin::PublicKey::from_slice(&op.recipient.bytes()).unwrap();
-        let p2wpk = bitcoin::Script::new_v0_p2wpkh(&user_address.wpubkey_hash().unwrap());
+        //let p2wpk = bitcoin::Script::new_v0_p2wpkh(&user_address.wpubkey_hash().unwrap());
+        let peg_out_output_stx = op.recipient.to_bitcoin_tx_out(op.amount);
+        let peg_out_script = Script::from_hex(&peg_out_output_stx.script_pubkey.to_hex()).unwrap();
         let peg_out_output = bitcoin::TxOut {
-            value: op.amount,
-            script_pubkey: p2wpk,
+            value: peg_out_output_stx.value,
+            script_pubkey: peg_out_script,
         };
         bitcoin::blockdata::transaction::Transaction {
             version: 0,
@@ -93,5 +94,37 @@ impl BitcoinWallet for FileBitcoinWallet {
             input: vec![peg_out_input],
             output: vec![peg_out_output],
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use blockstack_lib::burnchains::Txid;
+    use blockstack_lib::chainstate::stacks::address::{PoxAddress, PoxAddressType20};
+    use blockstack_lib::types::chainstate::BurnchainHeaderHash;
+    use blockstack_lib::util::secp256k1::MessageSignature;
+
+    use crate::peg_wallet::{BitcoinWallet, FileBitcoinWallet};
+    use crate::stacks_node::PegOutRequestOp;
+
+    #[test]
+    fn fufill_peg_out() {
+        let wallet = FileBitcoinWallet {};
+        let recipient = PoxAddress::Addr20(true, PoxAddressType20::P2WPKH, [0x01; 20]);
+        let peg_wallet_address = PoxAddress::Addr20(true, PoxAddressType20::P2WPKH, [0x01; 20]);
+        let req_op = PegOutRequestOp {
+            amount: 1000,
+            recipient: recipient,
+            signature: MessageSignature([0x00; 65]),
+            peg_wallet_address: peg_wallet_address,
+            fulfillment_fee: 0,
+            memo: vec![],
+            txid: Txid([0x04; 32]),
+            vtxindex: 0,
+            block_height: 0,
+            burn_header_hash: BurnchainHeaderHash([0x00; 32]),
+        };
+        let btc_tx = wallet.fulfill_peg_out(&req_op);
+        assert_eq!(btc_tx.output[0].value, 1000)
     }
 }
