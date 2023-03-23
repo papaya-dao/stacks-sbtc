3f452c0aef0357e9c3384463e5c0137361200d1d
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-03-22 22:41:43 -0700

    OMG

--- .install/bitcoin.sh
+++ .install/bitcoin.sh
@@ -1,8 +1,15 @@
 if [[ $OSTYPE == 'darwin'* ]]
 then
-    curl -o bitcoin-22.0-osx-signed.dmg https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-osx-signed.dmg
-    sudo hdiutil attach bitcoin-22.0-osx-signed.dmg
-    sudo installer -package /Volumes/Bitcoin-Core/Bitcoin-Qt.app -target /
+
+    # curl -o bitcoin-22.0-osx-signed.dmg https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-osx-signed.dmg
+    # sudo hdiutil attach bitcoin-22.0-osx-signed.dmg
+    # sudo installer -package /Volumes/Bitcoin-Core/Bitcoin-Qt.app -target /
+
+    # curl -o bitcoin-22.0-osx64.tar.gz https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-osx64.tar.gz
+    # tar xzf bitcoin-22.0-osx64.tar.gz
+
+    curl -o bitcoin.rb https://raw.githubusercontent.com/Homebrew/homebrew-core/fa6b4765d81016166f6de2bdad96cfe914c1439f/Formula/bitcoin.rb
+    brew install ./bitcoin.rb
 else
     curl -o bitcoin-22.0-x86_64-linux-gnu.tar.gz https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz
     tar xzf bitcoin-22.0-x86_64-linux-gnu.tar.gz
