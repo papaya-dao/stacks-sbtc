30d5e8bc8d7b08bbd3b57db67a5feb0c0966bd58
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-03-22 22:16:48 -0700

    install bitcoin

--- .install/macos-latest.sh
+++
@@ -1,3 +0,0 @@
-curl -o bitcoin-22.0-osx-signed.dmg https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-osx-signed.dmg
-sudo hdiutil attach bitcoin-22.0-osx-signed.dmg
-sudo installer -package /Volumes/bitcoin-22.0-osx-signed/bitcoin-22.0-osx-signed.pkg -target /
