02b63f1ca76778de502514b2f2622a2f1b707103
Stjepan Golemac <stjepan.golemac@gmail.com>
2023-05-09 10:37:09 +0200

    wip: Define function inputs and outputs

--- commit-reveal-reference/src/lib.rs
+++ commit-reveal-reference/src/lib.rs
@@ -1,18 +1,30 @@
-use bitcoin::{ScriptBuf, Transaction, Witness};
+use bitcoin::{Address as BitcoinAddress, ScriptBuf, Transaction, Witness};
+use blockstack_lib::types::chainstate::StacksAddress;
 
-struct PegInCommit {
-    commit_tx: Transaction,
-    witness_script: Witness,
+pub struct PegInCommitInput {
+    pub address: StacksAddress,
+    pub amount: u64,
 }
 
-struct PegOutRequestCommit {
-    commit_tx: Transaction,
-    witness_script: Witness,
-    recipient_scriptPubKey: ScriptBuf,
-    fulfillment_fee: u64,
+pub struct PegInCommitOutput {
+    pub address: BitcoinAddress,
+    pub witness_script: Witness,
 }
 
-pub fn peg_in_commit_tx() -> Transaction {
+pub struct PegOutRequestCommitInput {
+    pub recipient_script_pub_key: ScriptBuf,
+    pub amount: u64,
+    pub fulfillment_fee: u64,
+}
+
+pub struct PegOutRequestCommitOutput {
+    pub commit_tx: Transaction,
+    pub witness_script: Witness,
+    pub recipient_script_pub_key: ScriptBuf,
+    pub fulfillment_fee: u64,
+}
+
+pub fn peg_in_commit_tx(_input: PegInCommitInput) -> PegInCommitOutput {
     todo!();
 }
 
@@ -20,7 +32,7 @@ pub fn peg_in_reveal_tx() -> Transaction {
     todo!();
 }
 
-pub fn peg_out_request_commit_tx() -> Transaction {
+pub fn peg_out_request_commit_tx(_input: PegOutRequestCommitInput) -> PegOutRequestCommitOutput {
     todo!();
 }
 
@@ -30,8 +42,6 @@ pub fn peg_out_request_reveal_tx() -> Transaction {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
         let result = 2 + 2;
