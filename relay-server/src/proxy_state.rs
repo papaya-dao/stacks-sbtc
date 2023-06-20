10cee647fb7dab399384f9edf46c65847629090a
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-06-20 08:37:32 -0700

    improve code cov

--- relay-server/src/proxy_state.rs
+++ relay-server/src/proxy_state.rs
@@ -8,17 +8,14 @@ pub struct ProxyState<T: Call>(pub T);
 
 impl<T: Call> State for ProxyState<T> {
     fn get(&mut self, node_id: String) -> Result<Vec<u8>, Error> {
-        Ok(self
-            .0
-            .call(Request::new(
-                Method::GET,
-                format!("/?id={node_id}"),
-                Default::default(),
-                Default::default(),
-            ))?
-            .content)
+        let response = self.0.call(Request::new(
+            Method::GET,
+            format!("/?id={node_id}"),
+            Default::default(),
+            Default::default(),
+        ))?;
+        Ok(response.content)
     }
-
     fn post(&mut self, msg: Vec<u8>) -> Result<(), Error> {
         self.0.call(Request::new(
             Method::POST,
@@ -80,5 +77,16 @@ mod tests {
             "Msg # 2".as_bytes().to_vec(),
             state.get(4.to_string()).unwrap()
         );
+    }
+    struct BrokenServer();
+    impl Call for BrokenServer {
+        fn call(&mut self, _: Request) -> Result<yarpc::http::Response, Error> {
+            Err(Error::new(std::io::ErrorKind::NotConnected, ""))
+        }
+    }
+    #[test]
+    fn post_fail() {
+        let mut state = ProxyState(BrokenServer());
+        assert!(state.post("Msg # 0".as_bytes().to_vec()).is_err());
     }
 }
