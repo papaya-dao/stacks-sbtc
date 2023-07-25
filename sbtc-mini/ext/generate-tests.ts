c4e440c719c31bfdc154b825ee0f433638be68b3
jesus <jnajera1917@gmail.com>
2023-07-25 09:50:27 -0400

    new test generation script - v much wip w/ commented out tests while updating

--- sbtc-mini/ext/generate-tests.ts
+++ sbtc-mini/ext/generate-tests.ts
@@ -1,148 +1,276 @@
-import { Clarinet, Contract, Account } from 'https://deno.land/x/clarinet@v1.7.0/index.ts';
-
-const sourcebootstrapFile = './tests/bootstrap.ts';
+import { Clarinet, Contract, Account } from 'https://deno.land/x/clarinet@v1.5.4/index.ts';
 const targetFolder = '.test';
 
 const warningText = `// Code generated using \`clarinet run ./scripts/generate-tests.ts\`
 // Manual edits will be lost.`;
 
-const defaultDeps = `import { Clarinet, Tx, Chain, Account, Block, types } from 'https://deno.land/x/clarinet@v1.7.0/index.ts';
+function getContractName(contractId: string) {
+    return contractId.split('.')[1];
+}
+
+function isTestContract(contractName: string) {
+    return contractName.substring(contractName.length - 5) === "_test";
+}
+
+type UnwrapInfo = {
+  contractName: string;
+  functionName: string;
+  args: {type: string, value: string}[];
+};
+
+function extractUnwrapInfo(statement: string): UnwrapInfo | null {
+  const match = statement.match(/\(unwrap! \(contract-call\? \.(.+?) (.+?)(( .+?)*)\)/);
+  if (!match) return null;
+
+  const contractName = match[1];
+  const functionName = match[2];
+  const argStrings = match[3].split(' ').filter(Boolean);
+  const args = argStrings.map(arg => {
+      const uintMatch = arg.match(/u(\d+)/);
+      if (uintMatch) {
+          return { type: 'uint', value: uintMatch[1] };
+      }
+      return { type: 'raw', value: arg };
+  });
+
+  return {
+      contractName,
+      functionName,
+      args
+  };
+}
+
+function generateTxString(unwrapInfo: UnwrapInfo, sender: string): string {
+  let argStrings = unwrapInfo.args.map(arg => {
+      if(arg.type === 'uint') {
+          return `types.uint(${arg.value})`;
+      } else {
+          return `"${arg.value}"`;  // Modify this to handle other types as needed
+      }
+  }).join(', ');
+
+  return `Tx.contractCall("${unwrapInfo.contractName}", "${unwrapInfo.functionName}", [${argStrings}], ${sender})`;
+}
+
+
+const externalAnnotationsRegex = /^([ \t]{0,};;[ \t]{0,}@[\s\S]+?)\n[ \t]{0,}\(define-public[\s]+\((.+?)[ \t|)]/gm;
+const annotationsRegex = /^;;[ \t]{1,}@([a-z-]+)(?:$|[ \t]+?(.+?))$/;
+
+function extractExternalAnnotations(contractSource: string) {
+    // Creating an object to store annotations with the function name as the key
+    const externalFunctionAnnotations = {};
+    // Replace all carriage returns (still unsure exactly what these are) with empty strings & then matching against the externalAnnotationsRegex
+    const matches = contractSource.replace(/\r/g, "").matchAll(externalAnnotationsRegex);
+    // For each match (function)
+    for (const [, comments, functionName] of matches) {
+        // Create an object specfic to the function
+        externalFunctionAnnotations[functionName] = {};
+        // Split the comments into an array of lines to handle multiple annotations
+        const lines = comments.split("\n");
+        // Loop through each line
+        for (const line of lines) {
+            // Match the line against the annotationsRegex to make sure it is an annotation
+            const [, prop, value] = line.match(annotationsRegex) || [];
+            // If the line is an annotation
+            if (prop)
+                // Add the annotation to the object
+                externalFunctionAnnotations[functionName][prop] = value ?? true;
+        }
+    }
+    //console.log("externalFunctionAnnotations with function name as key: " + externalFunctionAnnotations);
+    return externalFunctionAnnotations;
+}
+
+// Each "Test" should be:
+// External/Preparation Comments -> extractExternalAnnotations
+// Function Signature (define-public (function-name args))
+// Array of Comments or Functions
+// Need to account for existing "prepare" function
+
+Clarinet.run({
+    async fn(accounts: Map<string, Account>, contracts: Map<string, Contract>) {
+        Deno.writeTextFile(`${targetFolder}/deps.ts`, generateDeps());
+
+        for (const [contractId, contract] of contracts) {
+            const contractName = getContractName(contractId);
+            if (contractName === "sbtc-stacking-pool_test") {
+                const externalAnnotations = extractExternalAnnotations(contract.source);
+                const code: string[][] = [];
+                code.push([
+                    warningText,
+                    ``,
+                    `import { Clarinet, Tx, Chain, Account, types, assertEquals, printEvents, bootstrap } from './deps.ts';`,
+                    ``
+                ]);
+                //console.log(contract.contract_interface.functions.reverse())
+                //console.log(contract.source)
+                const testFunctions = extractTestFunctions(contract.source)
+                let i = 0;
+                //console.log("test functions: " + testFunctions);
+                for (const functionName in testFunctions) {
+                  const functionObject = testFunctions[functionName];
+                  code.push([
+                    `Clarinet.test({
+                  name: "${functionName}",
+                  async fn(chain: Chain, accounts: Map<string, Account>) {
+                      const deployer = accounts.get("deployer")!;
+                      bootstrap(chain, deployer);`
+                  ]);
+                  for(const event of functionObject.events) {
+                      if(event.type === "unwrap") {
+                          const unwrapInfo = extractUnwrapInfo(event.statement);
+                          if (unwrapInfo) {
+                              const txString = generateTxString(unwrapInfo, 'deployer.address');
+                              code.push([
+                                              `let block${i} = chain.mineBlock([
+                                      ${txString}
+                                  ]);`
+                              ]);
+                              i++;
+                          }
+                      } else if (event.type === "mine-empty") {
+                          // Handle 'mine-empty'
+                          code.push([
+                            `chain.mineEmptyBlock(${event.blocks});`
+                        ]);
+                      }
+                  }
+                  code.push([
+                    `block${i-1}.receipts.map(({result}) => result.expectOk());
+    }
+                            });`
+                  ]);
+              }
+
+
+
+//                 for (const { name, access, args } of contract.contract_interface.functions.reverse()) {
+//                     const functionAnnotations = externalAnnotations[name] || {};
+//                     // console.log(name)
+//                     // console.log(access)
+//                     // console.log(args)
+//                     code.push([
+//                         `Clarinet.test({
+//     name: "${name}",
+//     async fn(chain: Chain, accounts: Map<string, Account>) {
+//         const deployer = accounts.get("deployer")!;
+//         bootstrap(chain, deployer);
+//         let block = chain.mineBlock([
+//             Tx.contractCall('${contractId}', '${name}', [], deployer.address)
+//         ]);
+//         block.receipts.map(({result}) => result.expectOk());
+//     }
+//                             });`
+// ]);
+//                 }
+
+                console.log(code);
+
+              Deno.writeTextFile(`${targetFolder}/${contractName}.ts`, code.flat().join("\n"));
+
+            }
+        }
+    }
+})
+
+function generateDeps() {
+	return `${warningText}
+	
+import { Clarinet, Tx, Chain, Account, Block, types } from 'https://deno.land/x/clarinet@v1.5.4/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
 
 export { Clarinet, Tx, Chain, types, assertEquals };
 export type { Account };
 
-const dirOptions = { strAbbreviateSize: Infinity, depth: Infinity, colors: true };
+const dirOptions = {strAbbreviateSize: Infinity, depth: Infinity, colors: true};
 
 export function printEvents(block: Block) {
-	block.receipts.map(({ events }) => events && events.map(event => console.log(Deno.inspect(event, dirOptions))));
-}
-`;
-
-function getContractName(contractId: string) {
-	return contractId.split('.')[1];
+	block.receipts.map(({events}) => events && events.map(event => console.log(Deno.inspect(event, dirOptions))));
 }
 
-function isTestContract(contractName: string) {
-	return contractName.substring(contractName.length - 5) === "_test";
+export const bootstrapContracts = [
+	'.sbtc-token',
+	'.sbtc-peg-in-processor',
+	'.sbtc-peg-out-processor',
+	'.sbtc-registry',
+	'.sbtc-stacking-pool',
+	'.sbtc-testnet-debug-controller',
+	'.sbtc-token'
+];
+
+export function bootstrap(chain: Chain, deployer: Account) {
+	const { receipts } = chain.mineBlock([
+		Tx.contractCall(
+			\`\${deployer.address}.sbtc-controller\`,
+			'upgrade',
+			[types.list(bootstrapContracts.map(contract => types.tuple({ contract, enabled: true })))],
+			deployer.address
+		)
+	]);
+	receipts[0].result.expectOk().expectList().map(result => result.expectBool(true));
+}`;
 }
 
-const functionRegex = /^([ \t]{0,};;[ \t]{0,}@[\s\S]+?)\n[ \t]{0,}\(define-public[\s]+\((.+?)[ \t|)]/gm;
-const annotationsRegex = /^;;[ \t]{1,}@([a-z-]+)(?:$|[ \t]+?(.+?))$/;
+type FunctionEvent = { type: "unwrap", statement: string } | { type: "mine-empty", blocks: number };
 
-function extractTestAnnotations(contractSource: string) {
-	const functionAnnotations = {};
-	const matches = contractSource.replace(/\r/g, "").matchAll(functionRegex);
-	for (const [, comments, functionName] of matches) {
-		functionAnnotations[functionName] = {};
-		const lines = comments.split("\n");
-		for (const line of lines) {
-			const [, prop, value] = line.match(annotationsRegex) || [];
-			if (prop)
-				functionAnnotations[functionName][prop] = value ?? true;
-		}
-	}
-	return functionAnnotations;
+type TestFunction = {
+  events: FunctionEvent[],
+};
+
+type TestFunctions = {
+  [key: string]: TestFunction
+};
+
+function extractTestFunctions(contractSource: string): TestFunctions {
+  const lines = contractSource.split('\n');
+  const testFunctions: TestFunctions = {};
+  let currentFunctionName: string | null = null;
+  let currentUnwrapStatement: string | null = null;
+  let unwrapBlock = false;
+
+  for (let line of lines) {
+    line = line.trim();
+
+    // Check for function definition
+    const functionMatch = line.match(/^\(define-public\s+\((test-[^\s\)]+)/);
+    if (functionMatch) {
+      currentFunctionName = functionMatch[1];
+      testFunctions[currentFunctionName] = {
+        events: [],
+      };
+      continue;
+    }
+
+    // If we're inside a function...
+    if (currentFunctionName) {
+      // Check for unwrap statements
+      if (unwrapBlock || line.startsWith('(unwrap!')) {
+        unwrapBlock = true;
+        currentUnwrapStatement = (currentUnwrapStatement || '') + line + '\n';
+        if (line.endsWith(')')) {
+          testFunctions[currentFunctionName].events.push({type: "unwrap", statement: currentUnwrapStatement.trim()});
+          currentUnwrapStatement = null;
+          unwrapBlock = false;
+        }
+        continue;
+      }
+
+      // Check for mine-empty blocks
+      const mineEmptyBlocksMatch = line.match(/^;;\s*@mine-empty\s*([0-9]+)/);
+      if (mineEmptyBlocksMatch) {
+        testFunctions[currentFunctionName].events.push({type: "mine-empty", blocks: parseInt(mineEmptyBlocksMatch[1])});
+        continue;
+      }
+
+      // Check for function end
+      if (line === ')') {
+        currentFunctionName = null;
+      }
+    }
+  }
+  //console.log("testFunction result: " + JSON.stringify(testFunctions, null, 2));
+  return testFunctions;
 }
 
-Clarinet.run({
-	async fn(accounts: Map<string, Account>, contracts: Map<string, Contract>) {
-		Deno.writeTextFile(`${targetFolder}/deps.ts`, defaultDeps);
-		Deno.writeTextFile(`${targetFolder}/bootstrap.ts`, await generateBootstrapFile(sourcebootstrapFile));
-
-		for (const [contractId, contract] of contracts) {
-			const contractName = getContractName(contractId);
-			if (!isTestContract(contractName))
-				continue;
-
-			const hasDefaultPrepareFunction = contract.contract_interface.functions.reduce(
-				(a, v) => a || (v.name === 'prepare' && v.access === 'public' && v.args.length === 0),
-				false);
-			const annotations = extractTestAnnotations(contract.source);
-
-			const code: string[][] = [];
-			code.push([
-				warningText,
-				``,
-				`import { Clarinet, Tx, Chain, Account, types, assertEquals, printEvents } from './deps.ts';`,
-				`import { bootstrap } from './bootstrap.ts';`,
-				``
-			]);
-
-			for (const { name, access, args } of contract.contract_interface.functions.reverse()) {
-				if (access !== 'public' || name.substring(0, 5) !== 'test-')
-					continue;
-				if (args.length > 0)
-					throw new Error(`Test functions cannot take arguments. (Offending function: ${name})`);
-				const functionAnnotations = annotations[name] || {};
-				if (hasDefaultPrepareFunction && !functionAnnotations.prepare)
-					functionAnnotations.prepare = 'prepare';
-				if (functionAnnotations['no-prepare'])
-					delete functionAnnotations.prepare;
-				code.push([generateTest(contractId, name, functionAnnotations)]);
-			}
-
-			Deno.writeTextFile(`${targetFolder}/${contractName}.ts`, code.flat().join("\n"));
-		}
-	}
-});
-
-type FunctionAnnotations = { [key: string]: string | boolean };
-
-function generatePrepareTx(contractPrincipal: string, annotations: FunctionAnnotations) {
-	return `Tx.contractCall('${contractPrincipal}', '${annotations['prepare']}', [], deployer.address)`;
-}
-
-function generateNormalMineBlock(contractPrincipal: string, testFunction: string, annotations: FunctionAnnotations) {
-	return `let block = chain.mineBlock([
-		${annotations['prepare'] ? `${generatePrepareTx(contractPrincipal, annotations)},` : ''}
-		Tx.contractCall('${contractPrincipal}', '${testFunction}', [], callerAddress)
-	]);`;
-}
-
-function generateSpecialMineBlock(mineBlocksBefore: number, contractPrincipal: string, testFunction: string, annotations: FunctionAnnotations) {
-	let code = ``;
-	if (annotations['prepare']) {
-		code = `let prepareBlock = chain.mineBlock([${generatePrepareTx(contractPrincipal, annotations)}]);
-		prepareBlock.receipts.map(({result}) => result.expectOk());
-		`;
-		if (annotations['print'] === 'events')
-			code += `\n\t\tprintEvents(prepareBlock);\n`;
-	}
-	if (mineBlocksBefore > 1)
-		code += `
-		chain.mineEmptyBlock(${mineBlocksBefore - 1});`;
-	return `${code}
-		let block = chain.mineBlock([Tx.contractCall('${contractPrincipal}', '${testFunction}', [], callerAddress)]);
-		${annotations['print'] === 'events' ? 'printEvents(block);' : ''}`;
-}
-
-function generateTest(contractPrincipal: string, testFunction: string, annotations: FunctionAnnotations) {
-	const mineBlocksBefore = parseInt(annotations['mine-blocks-before'] as string) || 0;
-	return `Clarinet.test({
-	name: "${annotations.name ? testFunction + ': ' + (annotations.name as string).replace(/"/g, '\\"') : testFunction}",
-	async fn(chain: Chain, accounts: Map<string, Account>) {
-		const deployer = accounts.get("deployer")!;
-		bootstrap && bootstrap(chain, deployer);
-		let callerAddress = ${annotations.caller ? (annotations.caller[0] === "'" ? `"${(annotations.caller as string).substring(1)}"` : `accounts.get('${annotations.caller}')!.address`) : `accounts.get('deployer')!.address`};
-		${mineBlocksBefore >= 1
-			? generateSpecialMineBlock(mineBlocksBefore, contractPrincipal, testFunction, annotations)
-			: generateNormalMineBlock(contractPrincipal, testFunction, annotations)}
-		block.receipts.map(({result}) => result.expectOk());
-	}
-});
-`;
-}
-
-async function generateBootstrapFile(bootstrapFile: string) {
-	let bootstrapSource = 'export function bootstrap(){}';
-	if (bootstrapFile) {
-		try {
-			bootstrapSource = await Deno.readTextFile(bootstrapFile);
-		}
-		catch (error) {
-			console.error(`Could not read bootstrap file ${bootstrapFile}`, error);
-		}
-	}
-	return `${warningText}\n\n${bootstrapSource}`;
-}
+//console.log(extractTestFunctions(contractSource));
+//console.log("testFunction result: " + JSON.stringify(testFunctions, null, 2));
--- sbtc-mini/tests/sbtc-stacking-pool_test.clar
+++ sbtc-mini/tests/sbtc-stacking-pool_test.clar
@@ -14,96 +14,130 @@
 (define-constant normal-transfer-period-len u100)
 (define-constant normal-penalty-period-len u100)
 
-;; @name Is protocol caller test (is not at first)
+
+;; @name Set protocol caller test
 (define-public (test-is-protocol-caller)
-	(let ((is-protocol-caller
-			(contract-call? .sbtc-stacking-pool is-protocol-caller)))
-		(asserts! (is-err is-protocol-caller) (err "Should not be a protocol caller at first"))
+	(begin 
+		(unwrap! (contract-call? .pox-3 allow-contract-caller .sbtc-stacking-pool none) (err u1))
+		;; @mine-empty 1
 		(ok true)
 	)
 )
 
-;; @name Get current cycle stacker/signer pool, should return none
-(define-public (test-get-current-cycle-pool-none)
-    (let ((pool
-			(contract-call? .sbtc-stacking-pool get-current-cycle-pool)))
-		(asserts! (is-none pool) (err  "Should have succeeded"))
-		(ok true)
-	)
-)
+;; @name Get current cycle
+;; (define-public (test-get-current-window)
+;; 	(begin
+;; 		(unwrap! (contract-call? .sbtc-stacking-pool get-current-window) (err u1))
+;; 		(ok true)
+;; 	)
+;; )
 
-;; @name Get specific cycle stacker/signer pool, should return none
-(define-public (test-get-cycle-pool-none)
-	(let ((pool
-			(contract-call? .sbtc-stacking-pool get-specific-cycle-pool u0)))
-		(asserts! (is-none pool) (err  "Should have succeeded"))
-		(ok true)
-	)
-)
+;;(unwrap! (contract-call? .sbtc-stacking-pool is-protocol-caller) (err u2))
+;; @name Querying volunteer can pre-register in cycle (n - 1) to register in cycle n
+;; @mine-blocks-before 70200
+;; (define-public (test-pre-register)
+;; 	(begin
+;; 		(unwrap! (contract-call? .pox-3 allow-contract-caller .sbtc-stacking-pool none) (err u1))
+;; 		;; @mine-empty 70200
+;; 		;;(unwrap! (contract-call? .sbtc-stacking-pool signer-pre-register u100000000000 { version: 0x06, hashbytes: 0x0011223344556699001122334455669900112233445566990011223344556699 }) (err u2))
+;; 		;; (unwrap!
+;; 		;; 	(contract-call? .sbtc-stacking-pool signer-pre-register-test)
+;; 		;; 	(err 0)
+;; 		;; )
+;; 		(ok true)
+;; 	)
+;; )
 
-;; @name Test current window at 3701 blocks to be voting
-;; @mine-blocks-before 3700
-(define-public (test-get-current-window-voting)
-	(let ((current-window (contract-call? .sbtc-stacking-pool get-current-window)))
-		(asserts! (is-eq current-window voting) (err current-window))
-		(ok true)
-	)
-)
+;; @name Is protocol caller test (is not at first)
+;; (define-public (test-is-protocol-caller)
+;; 	(let ((is-protocol-caller
+;; 			(contract-call? .sbtc-stacking-pool is-protocol-caller)))
+;; 		(asserts! (is-err is-protocol-caller) (err "Should not be a protocol caller at first"))
+;; 		(ok true)
+;; 	)
+;; )
 
-;; @name Test current window at 4001 blocks to be transfer
-;; @mine-blocks-before 4000
-(define-public (test-get-current-window-transfer)
-	(let ((current-window (contract-call? .sbtc-stacking-pool get-current-window)))
-		(asserts! (is-eq current-window transfer) (err current-window))
-		(ok true)
-	)
-)
+;; ;; @name Get current cycle stacker/signer pool, should return none
+;; (define-public (test-get-current-cycle-pool-none)
+;;     (let ((pool
+;; 			(contract-call? .sbtc-stacking-pool get-current-cycle-pool)))
+;; 		(asserts! (is-none pool) (err  "Should have succeeded"))
+;; 		(ok true)
+;; 	)
+;; )
 
-;; @name Test current window at 4101 blocks to be penalty
-;; @mine-blocks-before 4100
-(define-public (test-get-current-window-penalty)
-	(let ((current-window (contract-call? .sbtc-stacking-pool get-current-window)))
-		(asserts! (is-eq current-window penalty) (err current-window))
-		(ok true)
-	)
-)
+;; ;; @name Get specific cycle stacker/signer pool, should return none
+;; (define-public (test-get-cycle-pool-none)
+;; 	(let ((pool
+;; 			(contract-call? .sbtc-stacking-pool get-specific-cycle-pool u0)))
+;; 		(asserts! (is-none pool) (err  "Should have succeeded"))
+;; 		(ok true)
+;; 	)
+;; )
 
-;; @name Test current window at 4201 blocks to be registration
-;; because this is the first cycle of the sbtc.
-;; @mine-blocks-before 4200
-(define-public (test-get-current-window-registration)
-	(let ((current-window (contract-call? .sbtc-stacking-pool get-current-window)))
-		(asserts! (is-eq current-window registration) (err current-window))
-		(ok true)
-	)
-)
+;; ;; @name Test current window at 3701 blocks to be voting
+;; ;; @mine-blocks-before 3700
+;; (define-public (test-get-current-window-voting)
+;; 	(let ((current-window (contract-call? .sbtc-stacking-pool get-current-window)))
+;; 		(asserts! (is-eq current-window voting) (err current-window))
+;; 		(ok true)
+;; 	)
+;; )
 
-;; @name check cycle length
-;; @mine-blocks-before 3700
-(define-public (test-pox-3-cycle-length)
-	(let ((current-cycle (contract-call? .pox-3 current-pox-reward-cycle))
-            (current-cycle-burn-height (contract-call? .pox-3 reward-cycle-to-burn-height current-cycle))
-            (next-cycle (+ u1 (contract-call? .pox-3 current-pox-reward-cycle)))
-            (next-cycle-burn-height (contract-call? .pox-3 reward-cycle-to-burn-height next-cycle))
-			(start-voting-window (- next-cycle-burn-height (+ normal-voting-period-len normal-transfer-period-len normal-penalty-period-len)))
-            (start-transfer-window (- next-cycle-burn-height (+ normal-transfer-period-len normal-penalty-period-len)))
-            (start-penalty-window (- next-cycle-burn-height normal-penalty-period-len))
-)
-		(asserts! (is-eq start-voting-window u3700) (err start-voting-window))
-		(asserts! (is-eq start-transfer-window u4000) (err start-transfer-window))
-		(asserts! (is-eq next-cycle-burn-height u4200) (err next-cycle-burn-height))
-		(asserts! (is-eq burn-block-height u3701) (err burn-block-height))
-		(ok true)
-	)
-)
+;; ;; @name Test current window at 4001 blocks to be transfer
+;; ;; @mine-blocks-before 4000
+;; (define-public (test-get-current-window-transfer)
+;; 	(let ((current-window (contract-call? .sbtc-stacking-pool get-current-window)))
+;; 		(asserts! (is-eq current-window transfer) (err current-window))
+;; 		(ok true)
+;; 	)
+;; )
 
-;; @name Get default signer in cycle
-(define-public (test-get-signer-in-cycle)
-	;; call get-signer-in-cycle with unknown signer
-	(let ((signer
-			(contract-call? .sbtc-stacking-pool get-signer-in-cycle 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM u0)))
-		;; expect to return default values
-		(asserts! (is-eq u0 (get amount signer)) (err "Default signer must have amount u0"))
-		(ok true)
-	)
-)
+;; ;; @name Test current window at 4101 blocks to be penalty
+;; ;; @mine-blocks-before 4100
+;; (define-public (test-get-current-window-penalty)
+;; 	(let ((current-window (contract-call? .sbtc-stacking-pool get-current-window)))
+;; 		(asserts! (is-eq current-window penalty) (err current-window))
+;; 		(ok true)
+;; 	)
+;; )
+
+;; ;; @name Test current window at 4201 blocks to be registration
+;; ;; because this is the first cycle of the sbtc.
+;; ;; @mine-blocks-before 4200
+;; (define-public (test-get-current-window-registration)
+;; 	(let ((current-window (contract-call? .sbtc-stacking-pool get-current-window)))
+;; 		(asserts! (is-eq current-window registration) (err current-window))
+;; 		(ok true)
+;; 	)
+;; )
+
+;; ;; @name check cycle length
+;; ;; @mine-blocks-before 3700
+;; (define-public (test-pox-3-cycle-length)
+;; 	(let ((current-cycle (contract-call? .pox-3 current-pox-reward-cycle))
+;;             (current-cycle-burn-height (contract-call? .pox-3 reward-cycle-to-burn-height current-cycle))
+;;             (next-cycle (+ u1 (contract-call? .pox-3 current-pox-reward-cycle)))
+;;             (next-cycle-burn-height (contract-call? .pox-3 reward-cycle-to-burn-height next-cycle))
+;; 			(start-voting-window (- next-cycle-burn-height (+ normal-voting-period-len normal-transfer-period-len normal-penalty-period-len)))
+;;             (start-transfer-window (- next-cycle-burn-height (+ normal-transfer-period-len normal-penalty-period-len)))
+;;             (start-penalty-window (- next-cycle-burn-height normal-penalty-period-len))
+;; )
+;; 		(asserts! (is-eq start-voting-window u3700) (err start-voting-window))
+;; 		(asserts! (is-eq start-transfer-window u4000) (err start-transfer-window))
+;; 		(asserts! (is-eq next-cycle-burn-height u4200) (err next-cycle-burn-height))
+;; 		(asserts! (is-eq burn-block-height u3701) (err burn-block-height))
+;; 		(ok true)
+;; 	)
+;; )
+
+;; ;; @name Get default signer in cycle
+;; (define-public (test-get-signer-in-cycle)
+;; 	;; call get-signer-in-cycle with unknown signer
+;; 	(let ((signer
+;; 			(contract-call? .sbtc-stacking-pool get-signer-in-cycle 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM u0)))
+;; 		;; expect to return default values
+;; 		(asserts! (is-eq u0 (get amount signer)) (err "Default signer must have amount u0"))
+;; 		(ok true)
+;; 	)
+;; )
